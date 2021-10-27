1ddffbf9da86561b47dafc2bf3a03014ea4e5cc3
quangvuu96 <quangvu090699@gmail.com>
2021-10-27 22:04:02 +0700

    test module 2

---
+++ Module_2/src/ss1_introduce_java/thuc_hanh_SystemTime.java
@@ -0,0 +1,10 @@
+package ss1_introduce_java;
+
+import java.util.Date;
+
+public class thuc_hanh_SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is " + now);
+    }
+}
